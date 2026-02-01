9e6cc595b746dfb3aa017606adc44acde27a727d
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-01 18:16:40 +0000

    Fixed web history error

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Shop from "../views/Shop.vue";
+
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/shop", name: "Shop", component: Shop },
 ];
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
 export default router;
